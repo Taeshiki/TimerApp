46abd161e966aada8305abd4c78e9ba6d469254d
김태영 <99245719+Taeshiki@users.noreply.github.com>
2022-03-19 07:23:32 +0900

    ui구성및 버튼연결

---
+++ TimerApp/TimerApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by 김태영 on 2022/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var toggleButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
